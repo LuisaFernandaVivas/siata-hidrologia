591293888616f6da8373fd6737dcdb2643005610
alexa <marcae777>
2019-05-18 22:29:31 -0500

    responsive app

--- src/django-project-ui/src/basins/BasinManager.js
+++ src/django-project-ui/src/basins/BasinManager.js
@@ -20,7 +20,7 @@ class BasinManager extends Component {
         item : {},
         data : {}
       }
-
+ClassName
   toogleItem(value,filter){ //http request
     const endpoint = 'hidrologia/api/basin/'+value.slug
     let thisComp = this
@@ -67,50 +67,48 @@ class BasinManager extends Component {
           return '';
       }
     return (
-      <div>
+      <div className = "container">
         <div className = "col-sm-12">
-          <img src="/static/banner.jpg" style={{width: 1600, height: 553}} />
-        </div>
-        <div className="col-sm-6">
-          <br/>
-          <br/>
-          <div className="station-title">
-          {isEmpty(item)}
+          <div id = "banner">
+            <img src="/static/banner.jpg" className="img-fluid" alt=".."/>
           </div>
-        </div>
-        <div className="col-sm-12">
-          <div className="col-sm-12">
-            <div className = "row">
-              <div id = 'timechart-row' className ='col-sm-5'>
-                <WaterLevelChart data = {data} parameter = {'water_level'} color = {"#4C90CD"} />
-                <RadarRainChart data = {data} parameter = {'radar_rain'} color = {"#008b8b"}/>
-                <WaterSurfaceVelocityChart data = {data} parameter = {'water_surface_velocity'} color = {"black"}/>
-                <VideoChart camera_path = {item.camera_path}/>
-              </div>
-              <div id = 'picturechart-row' className ='col-sm-3'>
-                <PictureChart path={item.water_level_history_path} title={"Histórico de hidrógrafas"}/>
-                <PictureChart path={item.radar_rain_history_path} title={"Histórico de eventos de lluvia"}/>
-                <PictureChart path={item.statistical_model_path} title={"Modelo estadístico"}/>
-              </div>
-              <div id = 'container' className ='col-sm-4'>
-                <div className="chart-wrapper">
-                  <div className="chart-title">
-                    Mapa
-                  </div>
-                  <div className="chart-stage">
-                    <div>
-                      <MapChart item = {item}/>
-                    </div>
+          <div className="col-sm-6">
+            <br/>
+            <br/>
+            <div className="station-title">
+              {isEmpty(item)}
+            </div>
+          </div>
+          <div className = "row">
+            <div id = 'timechart-row' className ='col-sm-5'>
+              <WaterLevelChart data = {data} parameter = {'water_level'} color = {"#4C90CD"} />
+              <RadarRainChart data = {data} parameter = {'radar_rain'} color = {"#008b8b"}/>
+              <WaterSurfaceVelocityChart data = {data} parameter = {'water_surface_velocity'} color = {"black"}/>
+              <VideoChart camera_path = {item.camera_path}/>
+            </div>
+            <div id = 'picturechart-row' className ='col-sm-3'>
+              <PictureChart path={item.water_level_history_path} title={"Histórico de hidrógrafas"}/>
+              <PictureChart path={item.radar_rain_history_path} title={"Histórico de eventos de lluvia"}/>
+              <PictureChart path={item.statistical_model_path} title={"Modelo estadístico"}/>
+            </div>
+            <div id = 'container' className ='col-sm-4'>
+              <div className="chart-wrapper">
+                <div className="chart-title">
+                  Mapa
+                </div>
+                <div className="chart-stage">
+                  <div>
+                    <MapChart item = {item}/>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div id='basinchart' className='col-sm-12'>
+          <div id='basinchart'>
             <BasinChart show = {show} click={this.toogleItem}/>
           </div>
-        </div>
       </div>
+    </div>
     );
   }
 }
